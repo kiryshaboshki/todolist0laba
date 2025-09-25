2a374a35443cf6ad78215d67c8784741c6b5031d
kiryshaboshki <danilovkirill58@gmail.com>
2025-09-25 21:58:10 +1000

    галочки готовы, след этап даты и календарь

--- todolist0laba/MainWindow.xaml.cs
+++ todolist0laba/MainWindow.xaml.cs
@@ -1,35 +1,39 @@
 ﻿using System.Windows;
 using System.Collections.ObjectModel;
+using todolist0laba.Models;
 
 namespace todolist0laba
 {
     public partial class MainWindow : Window
     {
-        private ObservableCollection<string> tasks = new ObservableCollection<string>();
+        private ObservableCollection<TaskItem> tasks = new ObservableCollection<TaskItem>();
 
         public MainWindow()
         {
             InitializeComponent();
+
             TasksListBox.ItemsSource = tasks;
 
-            tasks.Add("Сделать лабу номер 0");
-            tasks.Add("поесть");
-            tasks.Add("добавить даты и т.д");
-            tasks.Add("проверить работу коммитов в VS");
+            tasks.Add(new TaskItem { Title = "Сделать лабу номер 0" });
+            tasks.Add(new TaskItem { Title = "поесть" });
+            tasks.Add(new TaskItem { Title = "добавить даты" });
+            tasks.Add(new TaskItem { Title = "добавить календарь" });
+            tasks.Add(new TaskItem { Title = "База данных?" });
+            tasks.Add(new TaskItem { Title = "Улучшить интерфейс(внешка)" });
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(NewTaskTextBox.Text))
             {
-                tasks.Add(NewTaskTextBox.Text);
+                tasks.Add(new TaskItem { Title = NewTaskTextBox.Text });
                 NewTaskTextBox.Text = "";
             }
         }
 
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            if (TasksListBox.SelectedItem is string selectedTask)
+            if (TasksListBox.SelectedItem is TaskItem selectedTask)
             {
                 tasks.Remove(selectedTask);
             }
