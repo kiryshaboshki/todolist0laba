c8b95b692be02624f9a48798cbe9a483277d6263
kiryshaboshki <danilovkirill58@gmail.com>
2025-09-25 21:25:37 +1000

    Добавьте файлы проекта.

---
+++ todolist0laba/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Collections.ObjectModel;
+
+namespace todolist0laba
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> tasks = new ObservableCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TasksListBox.ItemsSource = tasks;
+
+            tasks.Add("Сделать лабу номер 0");
+            tasks.Add("поесть");
+            tasks.Add("добавить даты и т.д");
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(NewTaskTextBox.Text))
+            {
+                tasks.Add(NewTaskTextBox.Text);
+                NewTaskTextBox.Text = "";
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TasksListBox.SelectedItem is string selectedTask)
+            {
+                tasks.Remove(selectedTask);
+            }
+        }
+    }
+}
