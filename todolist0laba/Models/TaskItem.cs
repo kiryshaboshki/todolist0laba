54e27c4309deafedf44bb604cd991aee5a43e9c3
kiryshaboshki <danilovkirill58@gmail.com>
2025-09-25 21:47:01 +1000

    начало создания галочек к задачам

---
+++ todolist0laba/Models/TaskItem.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace todolist0laba.Models
+{
+    public class TaskItem : INotifyPropertyChanged
+    {
+        private string _title;
+        private bool _isCompleted;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+
+            }
+        }
+
+        public bool IsCompleted
+        {
+            get { return _isCompleted; }
+            set
+            {
+                _isCompleted = value;
+                OnPropertyChanged(nameof(IsCompleted));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
